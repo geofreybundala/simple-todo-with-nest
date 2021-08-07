097fece7addf66919895697d3bac096f1d760354
geofreybundala <bundalageofrey2@gmail.com>
2021-08-07 12:41:40 +0300

    🎨 FEAT: add env on configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TodoModule } from './todo/todo.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TodoModule],
+  imports: [ConfigModule.forRoot(), TodoModule],
 })
 export class AppModule {}

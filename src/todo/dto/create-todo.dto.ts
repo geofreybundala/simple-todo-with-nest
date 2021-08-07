c714f7ca3c504b50bc3861342bc4c27489aa462f
geofreybundala <bundalageofrey2@gmail.com>
2021-08-07 20:09:26 +0300

    feat:Add validator class to DTO

--- src/todo/dto/create-todo.dto.ts
+++ src/todo/dto/create-todo.dto.ts
@@ -1,1 +1,11 @@
-export class CreateTodoDto {}
+import { IsNotEmpty, IsString, IsOptional } from 'class-validator';
+
+export class CreateTodoDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+}

2fc0b7404aafcdf2fa8feb09d57e8487769992d6
geofreybundala <bundalageofrey2@gmail.com>
2021-08-10 23:22:22 +0300

    feat:add test controller

--- src/todo/todo.controller.spec.ts
+++ src/todo/todo.controller.spec.ts
@@ -5,16 +5,36 @@ import { TodoService } from './todo.service';
 describe('TodoController', () => {
   let controller: TodoController;
 
+  const mockTodoService = {
+    create: jest.fn(dto =>{
+      return {
+        _id: 'wewe23231',
+        ...dto
+      }
+    })
+  }
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TodoController],
       providers: [TodoService],
-    }).compile();
+    })
+    .overrideProvider(TodoService)
+    .useValue(mockTodoService)
+    .compile();
 
     controller = module.get<TodoController>(TodoController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('it should create todo',()=> {
+      const  dto = {name:'test', description: 'hello test '}
+      expect(controller.create(dto)).toEqual({
+        _id:expect.any(String),
+        ...dto
+      })
   });
 });
